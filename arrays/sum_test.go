7b810f063bd9e7313c8d08e174c12b8c778922c6
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2024-05-31 21:22:55 +0300

    Add SummAll function

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 )
 
@@ -18,5 +19,20 @@ func TestSum(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %d want %d given %v", got, want, numbers)
+	}
+}
+
+func ExampleSumAll() {
+	sum_array := SumAll([]int{1, 2}, []int{4, 5})
+	fmt.Println(sum_array)
+	// Output: [3 9]
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
